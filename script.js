30a5686fadfb9154e70f2b7a7b8a7f4a1cf2ab34
kna27 <krisharora27@gmail.com>
2022-11-13 23:23:58 -0500

    Formatting

--- script.js
+++ script.js
@@ -1,12 +1,10 @@
+const skinSelect = document.getElementById("skinSelect");
+const skinSelectWeaponName = document.getElementById("skinChange_WeaponName");
+const skinSelectSelectedImage = document.getElementById("skinChange_SelectedImage");
+const skinSelectSkinOptions = document.getElementById("skinChange_SkinOptions");
 var skins = {};
 var equippedSkins = {};
-var skinSelect = document.getElementById("skinSelect");
-var skinSelectWeaponName = document.getElementById("skinChange_WeaponName");
-var skinSelectSelectedImage = document.getElementById("skinChange_SelectedImage");
-var skinSelectSkinOptions = document.getElementById("skinChange_SkinOptions");
 var highLightedSkin = "";
-
-skinSelect.style.visibility = "hidden";
 
 fetch("https://valorant-api.com/v1/weapons")
     .then(response => response.json())
@@ -27,7 +25,7 @@ fetch("https://valorant-api.com/v1/weapons")
         loadPage();
     }).catch(err => console.error(err));
 
-function removeMissingSkins() {
+const removeMissingSkins = () => {
     delete skins["Marshal"]["Sovereign Marshal"];
     delete skins["Guardian"]["Prime Guardian"];
     delete skins["Guardian"]["Sovereign Guardian"];
@@ -35,31 +33,34 @@ function removeMissingSkins() {
     delete skins["Melee"]["Melee"];
     Object.keys(skins).forEach(weapon => {
         delete skins[weapon]["Random Favorite Skin"];
-    })
-}
-
-function loadPage() {
-    Array.prototype.forEach.call(document.getElementsByClassName("weapon"), function (element) {
-        let weapon = element.className.split(" ").at(-1)
-        let nameDiv = document.createElement("div");
-        nameDiv.className = "weaponName";
-        nameDiv.innerHTML = weapon.toUpperCase();
-
-        let imgContainer = document.createElement("div");
-        imgContainer.setAttribute("onClick", "javascript: selectSkin(this);");
-        imgContainer.className = "imgContainer";
-
-        let img = document.createElement("img");
-        img.className = `weaponImg img${weapon}`;
-        img.innerHTML = weapon.toUpperCase();
-        img.src = skins[weapon][equippedSkins[weapon]];
-        element.appendChild(nameDiv);
-        imgContainer.appendChild(img);
-        element.appendChild(imgContainer);
     });
 }
 
-function selectSkin(weapon) {
+const loadPage = () => {
+    skinSelect.style.visibility = "hidden";
+    Array.prototype.forEach.call(document.getElementsByClassName("weapon"),
+        (element) => {
+            let weapon = element.className.split(" ").at(-1);
+            let nameDiv = document.createElement("div");
+            nameDiv.className = "weaponName";
+            nameDiv.innerHTML = weapon.toUpperCase();
+
+            let imgContainer = document.createElement("div");
+            imgContainer.setAttribute("onClick",
+                "javascript: selectSkin(this);");
+            imgContainer.className = "imgContainer";
+
+            let img = document.createElement("img");
+            img.className = `weaponImg img${weapon}`;
+            img.innerHTML = weapon.toUpperCase();
+            img.src = skins[weapon][equippedSkins[weapon]];
+            element.appendChild(nameDiv);
+            imgContainer.appendChild(img);
+            element.appendChild(imgContainer);
+        });
+}
+
+const selectSkin = (weapon) => {
     weapon = weapon.parentNode.className.split(" ").at(-1);
     highLightedSkin = equippedSkins[weapon];
     skinSelectSelectedImage.src = skins[weapon][equippedSkins[weapon]];
@@ -77,13 +78,13 @@ function selectSkin(weapon) {
     });
 }
 
-function titleCase(str) {
-    return str.toLowerCase().split(" ").map(function (word) {
+const titleCase = (str) => {
+    return str.toLowerCase().split(" ").map((word) => {
         return (word.charAt(0).toUpperCase() + word.slice(1));
     }).join(" ");
 }
 
-function equipSkin() {
+const equipSkin = () => {
     weapon = titleCase(skinSelectWeaponName.innerText);
     equippedSkins[weapon] = highLightedSkin;
     skinSelect.style.visibility = "hidden";
@@ -92,40 +93,44 @@ function equipSkin() {
     saveToLocalStorage();
 }
 
-function resetLoadout() {
-    Array.prototype.forEach.call(document.getElementsByClassName("weapon"), function (element) {
-        let weapon = element.className.split(" ").at(-1)
-        let skinImg = document.getElementsByClassName(`img${weapon}`)[0];
-        skinImg.src = skins[weapon][`Standard ${weapon}`];
-        equippedSkins[weapon] = `Standard ${weapon}`;
-        saveToLocalStorage();
-    });
+const resetLoadout = () => {
+    Array.prototype.forEach.call(document.getElementsByClassName("weapon"),
+        (element) => {
+            let weapon = element.className.split(" ").at(-1);
+            let skinImg = document.getElementsByClassName(`img${weapon}`)[0];
+            skinImg.src = skins[weapon][`Standard ${weapon}`];
+            equippedSkins[weapon] = `Standard ${weapon}`;
+            saveToLocalStorage();
+        });
 }
 
-function randomLoadout() {
-    Array.prototype.forEach.call(document.getElementsByClassName("weapon"), function (element) {
-        let weapon = element.className.split(" ").at(-1)
-        let skinImg = document.getElementsByClassName(`img${weapon}`)[0];
-        let randomSkin = `Standard ${weapon}`;
-        while (randomSkin === `Standard ${weapon}`) {
-            randomSkin = Object.keys(skins[weapon])[Math.floor(Math.random() * Object.keys(skins[weapon]).length)]
-        }
-        skinImg.src = skins[weapon][randomSkin];
-        equippedSkins[weapon] = randomSkin;
-        saveToLocalStorage();
-    });
+const randomLoadout = () => {
+    Array.prototype.forEach.call(document.getElementsByClassName("weapon"),
+        (element) => {
+            let weapon = element.className.split(" ").at(-1);
+            let skinImg = document.getElementsByClassName(`img${weapon}`)[0];
+            let randomSkin = `Standard ${weapon}`;
+            while (randomSkin === `Standard ${weapon}`) {
+                randomSkin = Object.keys(skins[weapon])
+                [Math.floor(Math.random() * Object.keys(skins[weapon]).length)];
+            }
+            skinImg.src = skins[weapon][randomSkin];
+            equippedSkins[weapon] = randomSkin;
+            saveToLocalStorage();
+        });
 }
 
-function highLight(skin) {
-    highLightedSkin = skin
-    skinSelectSelectedImage.src = skins[titleCase(skinSelectWeaponName.innerText)][skin];
+const highLight = (skin) => {
+    highLightedSkin = skin;
+    skinSelectSelectedImage.src =
+        skins[titleCase(skinSelectWeaponName.innerText)][skin];
 }
 
-function saveToLocalStorage() {
+const saveToLocalStorage = () => {
     localStorage.setItem("equippedSkins", JSON.stringify(equippedSkins));
 }
 
-function equipAllSkins() {
+const equipAllSkins = () => {
     if (localStorage.getItem("equippedSkins")) {
         equippedSkins = JSON.parse(localStorage.getItem("equippedSkins"));
     } else {
@@ -135,7 +140,7 @@ function equipAllSkins() {
     }
 }
 
-function missingSkins() {
+const missingSkins = () => {
     alert(`Due to API limitations, the following skins are unavailable:
     Nitro Odin 
     Snowfall Ares 
@@ -189,10 +194,11 @@ function missingSkins() {
     Prime Guardian
     Sovereign Guardian
     Luxe Knife
-    `)
+    `);
 }
 
-function about() {
-    alert(`This is a tool to help you create a loadout for Valorant without paying for skins.
-This website can help you to stop wasting money on skins and decide which ones you really want.`)
+const about = () => {
+    alert(
+        `This tool helps you create a Valorant loadout without paying for skins.
+It can help you to stop wasting money on skins and decide which ones you really want.`);
 }
